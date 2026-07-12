36c82fe4cb754dc88bbf994c2ded67c57a187350
Chavanmanoj <manojwchavan02@gmail.com>
2026-07-12 12:37:01 +0530

    redeclaration and reassignment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //declaration and initialization
 
-var a; //declare
+/*var a; //declare
 var a = 32 ;//initialize
 
 //add in the window when we create with the var 
@@ -16,9 +16,10 @@ console.log(username);
 
 //now let 
 let b = 42;
-/*let b = 75; we cannot declare variable 
+let b = 75; we cannot declare variable 
 with let again in the same scope */
-console.log(b);
+
+/*console.log(b);
 
 //const
 
@@ -38,4 +39,18 @@ var x = 10 ;
 {
     var y =20; //var function scope
     let w = 20; //block scope we use in the block only
-}
+}*/
+
+//reassignment redeclaration and initialization
+
+var a = 10;
+a = 32;
+//redeclaration with the var is possible
+
+let b = 32;
+b = 63;// reassignment with let is possible
+//redeclarement with let is not possible
+
+const c = 3.14;
+//reassignment with const is not possible
+//redeclaration with const is not possible
