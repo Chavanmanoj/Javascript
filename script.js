7761ede7def19157e89e82d87090b12b0a668995
Chavanmanoj <manojwchavan02@gmail.com>
2026-07-12 12:19:24 +0530

    var let const

---
+++ script.js
@@ -0,0 +1,25 @@
+//var let const
+
+//declaration and initialization
+
+var a; //declare
+var a = 32 ;//initialize
+
+//add in the window when we create with the var 
+// function soped hota hai var
+
+var username = "manoj";
+//console.log(username);
+
+var username = "ihcus"; //op is ihcus of the username
+console.log(username);
+
+//now let 
+let b = 42;
+/*let b = 75; we cannot declare variable 
+with let again in the same scope */
+console.log(b);
+
+//const
+
+const c =3.14 ; 
