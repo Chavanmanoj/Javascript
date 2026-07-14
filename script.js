cdb8f6dd8353fb0289aa44cb90dcbeb84cdef053
Chavanmanoj <manojwchavan02@gmail.com>
2026-07-14 23:15:34 +0530

    fucntion statement and expression

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function dance(){
 }
 
 dance();
+
+let fnc = function(){
+    console.log('i am hero');
+}
+fnc();
+
+// function statements
+
+function hero(){
+    console.log('i am hero');
+}
+
+//function expressions
+
+let hero1 = function(){
+    console.log('i am hero');
+}
