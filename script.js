24e0b56d7f0e0e826927cb338dd214b4fa16527e
Chavanmanoj <manojwchavan02@gmail.com>
2026-07-14 23:07:51 +0530

    function basic

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
+//functions
 
-
-// print first 5 odd numbers
-
-let count = 0;
-for (let i = 1 ; i < 100 ; i++){
-    if(i % 2 === 1) {
-        count++;
-        console.log(i);
-        if(count === 5) break;
-    }
+function dance(){
+    console.log('i am hero');
 }
+
+dance();
