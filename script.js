c201f17632cda2cc01586a9d021446ca4315d162
Chavanmanoj <manojwchavan02@gmail.com>
2026-07-14 23:18:13 +0530

    fucntion fat arrow function

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-//functions
+// fat arrow function
 
-function dance(){
-    console.log('i am hero');
+let fnc = () => {
+    console.log("Hello World");
 }
 
-dance();
-
-let fnc = function(){
-    console.log('i am hero');
-}
-fnc();
-
-// function statements
-
-function hero(){
-    console.log('i am hero');
-}
-
-//function expressions
-
-let hero1 = function(){
-    console.log('i am hero');
-}
