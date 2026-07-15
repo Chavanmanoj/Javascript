fb2234c60c98180ea013441b1351064686e1777e
Chavanmanoj <manojwchavan02@gmail.com>
2026-07-15 23:15:05 +0530

    lexical scoping

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-//first class functions
-let myFunction = function() {
-    console.log("Hello World");
+// lexical scoping
+function abcd() {
+    let a = 10;
+    function inner() {
+        let b = 20;
+        function innerMost() {
+            let c = 30;
+            console.log(a + b + c); // Accessing variables from outer scopes
+        }
+    }
 }
-myFunction();
